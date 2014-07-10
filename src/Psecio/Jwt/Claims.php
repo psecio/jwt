c222eb178e7bcef7503b96f5456fecd51bd3cf80
Chris Cornutt <enygma@phpdeveloper.org>
2014-07-10 18:39:15 -0500

    adding docblock to the claims class

--- src/Psecio/Jwt/Claims.php
+++ src/Psecio/Jwt/Claims.php
@@ -4,32 +4,77 @@ namespace Psecio\Jwt;
 
 abstract class Claims
 {
+	/**
+	 * Claim type
+	 * @var string
+	 */
 	protected $type;
+
+	/**
+	 * Claim value
+	 * @var string
+	 */
 	protected $value;
 
+	/**
+	 * Initialize the claim with the given value
+	 *
+	 * @param string $value Claim value
+	 */
 	public function __construct($value)
 	{
 		$this->setValue($value);
 	}
+
+	/**
+	 * Set the claim value
+	 *
+	 * @param string $value Claim value
+	 * @return \Psecio\Jwt\Claims instance
+	 */
 	public function setValue($value)
 	{
 		$this->value = $value;
 		return $this;
 	}
+
+	/**
+	 * Get the current value
+	 *
+	 * @return string Claim value
+	 */
 	public function getValue()
 	{
 		return $this->value;
 	}
+
+	/**
+	 * Set the claim type
+	 *
+	 * @param string $type Claim type
+	 * @return \Psecio\Jwt\Claims instance
+	 */
 	public function setType($type)
 	{
 		$this->type = $type;
 		return $this;
 	}
+
+	/**
+	 * Get the current claim type
+	 *
+	 * @return string Claim type
+	 */
 	public function getType()
 	{
 		return $this->type;
 	}
 
+	/**
+	 * Transform claim data into an array
+	 *
+	 * @return array Claim data
+	 */
 	public function toArray()
 	{
 		return array(
