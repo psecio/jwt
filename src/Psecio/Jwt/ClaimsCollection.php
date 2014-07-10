6fa774e3c6507142e18f1cad5f3cf185d9bfdbbd
Chris Cornutt <enygma@phpdeveloper.org>
2014-07-10 18:42:08 -0500

    adding docblock to claims collection

--- src/Psecio/Jwt/ClaimsCollection.php
+++ src/Psecio/Jwt/ClaimsCollection.php
@@ -4,14 +4,29 @@ namespace Psecio\Jwt;
 
 class ClaimsCollection
 {
+	/**
+	 * Set of Claims
+	 * @var array
+	 */
 	private $claims = array();
 
+	/**
+	 * Add a claim to the collection
+	 *
+	 * @param \Psecio\Jwt\Claims $claim Claim instance
+	 * @return \Psecio\Jwt\ClaimsCollection instance
+	 */
 	public function add(\Psecio\Jwt\Claims $claim)
 	{
 		$this->claims[] = $claim;
 		return $this;
 	}
 
+	/**
+	 * Convert the collection to a nested array
+	 *
+	 * @return array Contents of the collection
+	 */
 	public function toArray()
 	{
 		$data = array();
