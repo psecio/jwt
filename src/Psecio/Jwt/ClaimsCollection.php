4dfb84baf6e27cd1b02aed6c82e5be82df3c55e2
Chris Cornutt <chris.cornutt@hp.com>
2015-02-11 14:20:24 -0600

    making the claims collection countable and iterable

--- src/Psecio/Jwt/ClaimsCollection.php
+++ src/Psecio/Jwt/ClaimsCollection.php
@@ -2,7 +2,7 @@
 
 namespace Psecio\Jwt;
 
-class ClaimsCollection
+class ClaimsCollection implements \Countable, \Iterator
 {
 	/**
 	 * Set of Claims
@@ -35,4 +35,70 @@ class ClaimsCollection
 		}
 		return $data;
 	}
+
+	/**
+     * Current position in data (used in Iterator)
+     * @var integer
+     */
+    private $position = 0;
+
+    // For Countable interface
+    /**
+     * Return a count of the current data
+     *
+     * @return integer Count result
+     */
+    public function count()
+    {
+        return count($this->claims);
+    }
+
+    // For Iterator
+    /**
+     * Return the current item in the set
+     *
+     * @return mixed Current data item
+     */
+    public function current()
+    {
+        return $this->claims[$this->position];
+    }
+
+    /**
+     * Return the current key (position) value
+     *
+     * @return integer Position value
+     */
+    public function key()
+    {
+        return $this->position;
+    }
+
+    /**
+     * Get the next position value
+     *
+     * @return integer Next position
+     */
+    public function next()
+    {
+        return ++$this->position;
+    }
+
+    /**
+     * Rewind to the beginning of the set (position = 0)
+     */
+    public function rewind()
+    {
+        $this->position = 0;
+    }
+
+    /**
+     * See if the requested position exists in the data
+     *
+     * @return boolean Exists/doesn't exist
+     */
+    public function valid()
+    {
+        return isset($this->claims[$this->position]);
+    }
 }
