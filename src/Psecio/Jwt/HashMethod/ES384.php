276048ef26fe321ae0f7c75495aa72460dee7700
Chris Cornutt <chris.cornutt@hp.com>
2015-02-11 11:05:18 -0600

    adding other hash method generation classes

---
+++ src/Psecio/Jwt/HashMethod/ES384.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Psecio\Jwt\HashMethod;
+
+class ES384 extends \Psecio\Jwt\HashMethod
+{
+    protected $keyType = OPENSSL_KEYTYPE_RSA;
+
+    public function getAlgorithm()
+    {
+        if (version_compare(phpversion(), '5.4.8', '<')) {
+            return 'SHA384';
+        } else {
+            if (!extension_loaded('openssl')) {
+                throw new \InvalidArgumentException('You cannot use this hashing method without OpenSSL support');
+            }
+            return OPENSSL_ALGO_SHA384;
+        }
+    }
+}
